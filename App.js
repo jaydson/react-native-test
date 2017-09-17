c0eb7275f2963306335ea3debc0f96ea8cfb59e3
Jaydson Gomes <jayalemao@gmail.com>
2017-09-17 10:20:16 -0700

    Moving the logic to a react component

--- App.js
+++ App.js
@@ -1,54 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View, Alert, Button, Image } from 'react-native';
+import FacebookLogin from './FacebookLogin';
 
-async function logIn() {
-  const APP_ID = '375722322841966';
-
-  const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync(APP_ID, {
-      permissions: ['public_profile'],
-  });
-
-  if (type === 'success') {
-    const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
-    const userData = await response.json();
-    const userPic = `https://graph.facebook.com/v2.10/${userData.id}/picture?access_token=${token}`;
-    userData.pic = userPic;
-    return userData;
-  }
-}
-
-export default class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {data: null}
-    this.handleLogin = this.handleLogin.bind(this);
-  }
-
-  handleLogin = async function() {  
-    const data = await logIn();
-    this.setState(data);
-  }
-  
+export default class App extends React.Component {  
   render() {
     return (
-      <View style={styles.container}>
-        <Text>{this.state.name ? `Welcome ${this.state.name}` : 'Your\'e not logged yet my friend!'}</Text>
-        <Button onPress={this.handleLogin} title="FB Login" />
-        <Image
-            style={{width: 50, height: 50}}
-            source={{uri: this.state.pic ? this.state.pic : 'https://facebook.github.io/react/img/logo_og.png'}}
-          />
-      </View>
+      <FacebookLogin 
+        appId='375722322841966' 
+        textMessage='Please login in our awesome app!'
+        imagePlaceholder='https://facebook.github.io/react/img/logo_og.png' />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ FacebookLogin.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { StyleSheet, Text, View, Alert, Button, Image } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+async function logIn(appId) {
+  if (!appId) { 
+    throw new Execption('You should provide your Facebook App Id');
+  }
+  const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync(appId, {
+      permissions: ['public_profile'],
+  });
+
+  if (type === 'success') {
+    const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
+    const userData = await response.json();
+    const userPic = `https://graph.facebook.com/v2.10/${userData.id}/picture?access_token=${token}`;
+    userData.pic = userPic;
+    return userData;
+  }
+}
+
+export default class FacebookLogin extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {data: null}
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleLogin = async function() {
+    const data = await logIn(this.props.appId);
+    this.setState(data);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.name ? `Welcome ${this.state.name}` : this.props.textMessage}</Text>
+        <Button onPress={this.handleLogin} title="FB Login" />
+        <Image
+            style={{width: 50, height: 50}}
+            source={{uri: this.state.pic ? this.state.pic : this.props.imagePlaceholder}}
+          />
+      </View>
+    );
+  }
+}
